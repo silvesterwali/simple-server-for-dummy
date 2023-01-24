1d2ab25603391e030a19ea7efb6c60eb25f1d4a7
Silvester Wali <silvesterlhwali123@gmail.com>
2023-01-24 16:15:11 +0800

    fix:cors

--- app.js
+++ app.js
@@ -8,7 +8,21 @@ var usersRouter = require('./routes/users');
 var postRouter=require("./routes/posts")
 var app = express();
 
-app.use(cors({origin:"*"}))
+app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, Accept, Origin, Referer, User-Agent, Content-Type, Authorization');
+   
+    // intercept OPTIONS method
+    if (req.method === 'OPTIONS') {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+});
+app.use(cors())
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
