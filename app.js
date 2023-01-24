d8b728779ef50f6c6e74ca7fd57db288176e54de
Silvester Wali <silvesterlhwali123@gmail.com>
2023-01-24 15:57:52 +0800

    feat: adding cors

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var cors = require('cors')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var postRouter=require("./routes/posts")
 var app = express();
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
